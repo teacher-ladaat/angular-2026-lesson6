df5c0d877f1943a43f112a075f9c425a47275d8b
teacher <teacher@gmail.com>
2026-04-29 16:35:43 +0300

    refactor: comment out StudentTemplateFormComponent import in app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { StudentTemplateFormComponent } from "./student-template-form/student-template-form.component";
+// import { StudentTemplateFormComponent } from "./student-template-form/student-template-form.component";
 import { StudentsListComponent } from "./students-list/students-list.component";
 import { StudentsReactiveFormComponent } from "./students-reactive-form/students-reactive-form.component";
 
 @Component({
   selector: 'app-root',
-  imports: [StudentTemplateFormComponent, StudentsListComponent, StudentsReactiveFormComponent],
+    imports: [
+        // StudentTemplateFormComponent,
+        StudentsListComponent, StudentsReactiveFormComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
